e48d463c61bc6f2ae8a32504b4a21913796dc80c
Nikhil <nikhiljose18@gmail.com>
2022-09-16 23:29:53 +0530

    env variable file added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -18,9 +19,11 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 // add the plugin to the schema before creating new mongoose model.
-const secret = "Thisisourlittlesecret";
 //you can specify the fields you want to encrypt.
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
